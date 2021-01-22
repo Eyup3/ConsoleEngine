836720e1a273dd5d2445a69e65472ce080deacde
Eyüp Öztürk <VenoxUltra@outlook.de>
2021-01-22 14:57:03 +0100

    Projektdateien hinzufügen.

---
+++ ConsoleGameEngine/ConsoleEngine.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ConsoleEngine
+{
+    public abstract class ConsoleEngine
+    {
+
+        /// <summary>
+        /// Change to false to quit
+        /// </summary>
+        public bool run = true;
+
+        public abstract void Loop(decimal elapsedTime);
+        public abstract void Setup();
+
+        //Setup to get elapsedTime
+        private long startTime = 0;
+        private long endTime = 0;
+        private decimal elapsedTime = 0;
+
+
+        public void Start()
+        {
+
+            Setup();
+            while (run)
+            {
+                startTime = DateTime.Now.Ticks;
+
+                Loop(elapsedTime);
+
+                endTime = DateTime.Now.Ticks;
+                elapsedTime = ((endTime - startTime) / 10000m) / 1000m;
+            }
+        }
+    }
+}
---
+++ ConsoleGameEngine/Input.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace ConsoleEngine
+{
+    public static class Input
+    {
+        [DllImport("User32.dll")]
+        private static extern int GetAsyncKeyState(Int32 i);
+
+        /// <summary>
+        /// Get state of ConsoleKey
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public static bool GetKeyState(ConsoleKey key)
+        {
+            if (GetAsyncKeyState((int)key) != 0)
+            {
+                return true;
+            }
+            else { return false; }
+        }
+
+    }
+}
