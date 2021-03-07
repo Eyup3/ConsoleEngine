ec76f241d1b7345de2215adc8ab595c68a93bee3
Eyup3 <eyup.ozturk@outlook.de>
2021-03-07 17:06:28 +0100

    Restore Test

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using ConsoleEngine;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    class SS : ConsoleEngine.ConsoleEngine
+    {
+        protected override void Loop(decimal elapsedTime)
+        {
+            
+        }
+
+        protected override void Setup()
+        {
+        }
+    }
+}
